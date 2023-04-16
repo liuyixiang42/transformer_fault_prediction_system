59777130b3728d89b0ca99ee3447ecc0a8c65685
liuyixiang <576537490@qq.com>
2023-04-17 00:07:16 +0800

    init

--- src/view/LoginView.js
+++ src/view/LoginView.js
@@ -1,11 +1,16 @@
 import React from "react";
 import WrappedLoginForm from "../components/LoginForm";
 import { withRouter } from "react-router-dom";
+import { Typography } from 'antd';
+const { Title } = Typography;
 
 class LoginView extends React.Component {
   render() {
     return (
       <div className="login-page">
+        <div className="login-title">
+          <h>工业变压器故障分析预测系统</h>
+        </div>
         <div className="login-container">
           <div className="login-box">
             <h1 className="page-title">登录</h1>

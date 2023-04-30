ac160ef834202e6257a32a8d9b7b98f2c6daf031
liuyixiang <576537490@qq.com>
2023-04-30 18:39:40 +0800

    init

--- src/view/TransformerHomeView.js
+++ src/view/TransformerHomeView.js
@@ -21,60 +21,60 @@ class TransformerHomeView extends React.Component {
           <HeaderInfo />
         </Header>
         <Layout style={{margin: "0 auto"}}>
-            {/* <Space size={16}> */}
-                <Row gutter={[16, 16]}>
-                    <Col span={8}>
-                        <Button style={{width: "400px", height: "200px", fontSize: "30px"}} shape="round">
-                            <LineChartOutlined />
-                            <strong>变压器线圈数据故障分析</strong>
-                            <p style={{fontSize: "20px"}}>
-                                <br />
-                                根据变压器的线圈时序数据分析变压器
-                                <br />
-                                可能存在的故障类型
-                            </p>
-                        </Button>
-                    </Col>
-                    <Col span={8}>
-                        <Button style={{width: "400px", height: "200px", fontSize: "30px"}} shape="round">
-                            <LineChartOutlined />
-                            <strong>变压器油气数据故障分析</strong>
-                            <p style={{fontSize: "20px"}}>
-                                <br />
-                                根据变压器的油气时序数据分析变压器
-                                <br />
-                                可能存在的故障类型
-                            </p>
-                        </Button>
-                    </Col>
-                    <Col span={8}>
-                        <Button style={{width: "400px", height: "200px", fontSize: "30px"}} shape="round">
-                            <BarChartOutlined />
-                            <strong>变压器时序数据预测</strong>
-                            <p style={{fontSize: "20px"}}>
-                                <br />
-                                根据变压器的时序数据预测变压器
-                                <br />
-                                未来的运行状态
-                            </p>
-                        </Button>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col span={8}>
-                        <Button style={{width: "400px", height: "200px", fontSize: "30px"}} shape="round">
-                            <DotChartOutlined />
-                            <strong>变压器故障数据增强</strong>
-                            <p style={{fontSize: "20px"}}>
-                                <br />
-                                根据变压器的故障时序数据样本
-                                <br />
-                                生成相同故障类型的不同时序数据
-                            </p>
-                        </Button>
-                    </Col>
-                </Row>
-            {/* </Space> */}
+            <br /><br />
+            <Row gutter={[24, 24]}>
+                <Col span={8}>
+                    <Button style={{width: "400px", height: "200px", fontSize: "30px"}} shape="round">
+                        <LineChartOutlined />
+                        <strong>变压器线圈数据故障分析</strong>
+                        <p style={{fontSize: "20px"}}>
+                            <br />
+                            根据变压器的线圈时序数据分析变压器
+                            <br />
+                            可能存在的故障类型
+                        </p>
+                    </Button>
+                </Col>
+                <Col span={8}>
+                    <Button style={{width: "400px", height: "200px", fontSize: "30px"}} shape="round">
+                        <LineChartOutlined />
+                        <strong>变压器油气数据故障分析</strong>
+                        <p style={{fontSize: "20px"}}>
+                            <br />
+                            根据变压器的油气时序数据分析变压器
+                            <br />
+                            可能存在的故障类型
+                        </p>
+                    </Button>
+                </Col>
+                <Col span={8}>
+                    <Button style={{width: "400px", height: "200px", fontSize: "30px"}} shape="round">
+                        <BarChartOutlined />
+                        <strong>变压器时序数据预测</strong>
+                        <p style={{fontSize: "20px"}}>
+                            <br />
+                            根据变压器的时序数据预测变压器
+                            <br />
+                            未来的运行状态
+                        </p>
+                    </Button>
+                </Col>
+            </Row>
+            <br /><br />
+            <Row>
+                <Col span={8}>
+                    <Button style={{width: "400px", height: "200px", fontSize: "30px"}} shape="round">
+                        <DotChartOutlined />
+                        <strong>变压器故障数据增强</strong>
+                        <p style={{fontSize: "20px"}}>
+                            <br />
+                            根据变压器的故障时序数据样本
+                            <br />
+                            生成相同故障类型的不同时序数据
+                        </p>
+                    </Button>
+                </Col>
+            </Row>
         </Layout>
       </Layout>
     );
